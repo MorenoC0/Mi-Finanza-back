741c01bf6c09e75bbb370819f9f711e1f740026d
cris25nkl <cristian25florez@gmail.com>
2025-06-18 16:45:25 -0500

    CORS V.1.2

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
     origin: "https://jade-cendol-7c3b5b.netlify.app/",  // Acepta solo el front local
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
   await app.listen(process.env.PORT ?? 3000);
